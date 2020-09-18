3676541125206c36cea735b017b14e96ce9e944d
gbenedetti22 <gabriele.benedetti22@gmail.com>
2020-09-18 22:23:16 +0200

    L App funziona, le "cose da fare" vengono aggiunte quando si clicca il "+". Bisogna solo abbellire il tutto

--- app.js
+++ app.js
@@ -4,7 +4,27 @@ const todoList=document.querySelector('.todo-list');
 
 function addTodo(event) {
     event.preventDefault();
+    const todoDiv=document.createElement('div');
+    todoDiv.classList.add("todo");
 
+    const newTodo = document.createElement('li');
+    newTodo.innerText=todoInput.value;
+    newTodo.classList.add("todo-item");
+    todoDiv.appendChild(newTodo);
+
+    //Aggiungere un Button per il completamento
+    const completedButton = document.createElement("button");
+    completedButton.innerHTML= "<i class='fas fa-check'></i>"
+    completedButton.classList.add("complete-btn");
+    todoDiv.appendChild(completedButton);
+
+    //Aggiungere un Button per cancellare il Todo
+    const trashButton = document.createElement("button");
+    trashButton.innerHTML= "<i class='fas fa-trash'></i>"
+    trashButton.classList.add("complete-btn");
+    todoDiv.appendChild(trashButton);
+
+    todoList.appendChild(todoDiv);
 }
 
 todoButton.addEventListener('click',addTodo);
