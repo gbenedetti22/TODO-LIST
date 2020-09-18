a3fb2938489bd20f754bb3755ed18b98acdc2dfa
gbenedetti22 <gabriele.benedetti22@gmail.com>
2020-09-18 22:00:27 +0200

    HTML fatto (abbellito con CSS) e variabili Javascript inizializzate

---
+++ app.js
@@ -0,0 +1,11 @@
+const todoInput=document.querySelector('.todo-input');
+const todoButton=document.querySelector('.todo-button');
+const todoList=document.querySelector('.todo-list');
+
+function addTodo(event) {
+    event.preventDefault();
+
+}
+
+todoButton.addEventListener('click',addTodo);
+
