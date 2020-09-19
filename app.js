3bfd7e1e9de738de88ff161cd719322e36a4b620
gbenedetti22 <gabriele.benedetti22@gmail.com>
2020-09-19 22:47:44 +0200

    Aggiunta possibilità di vedere solo i Todo completati o quelli non completati, tramite un menù a tendina

--- app.js
+++ app.js
@@ -1,11 +1,12 @@
 const todoInput=document.querySelector('.todo-input');
 const todoButton=document.querySelector('.todo-button');
 const todoList=document.querySelector('.todo-list');
+const filterOption=document.querySelector('.filter-todo');
 
 function addTodo(event) {
     event.preventDefault();
 
-    if(todoInput.value===""){
+    if(todoInput.value === ""){
         alert("Hey devi inserire qualcosa da fare!");
         return;
     }
@@ -32,7 +33,7 @@ function addTodo(event) {
     todoList.appendChild(todoDiv);
     todoInput.value ="";
 }
-function deleteCheck(event) {
+function deleteTodo(event) {
     const item=event.target;
     if(item.classList[0]==="trash-btn"){
         item.parentElement.classList.add("fall");
@@ -46,6 +47,32 @@ function deleteCheck(event) {
     }
 }
 
-todoList.addEventListener("click", deleteCheck);
+function filterTodo(e){
+    const todos=todoList.childNodes;
+    todos.forEach(function(todo){
+        switch (e.target.value) {
+            case "all":
+                todo.style.display="flex";
+                break;
+            case "completed":
+                if(todo.classList.contains("completed")){
+                    todo.style.display="flex";
+                }else{
+                    todo.style.display="none";
+                }
+                break;
+            case "uncompleted":
+                if(!todo.classList.contains("completed")){
+                    todo.style.display="flex";
+                }else{
+                    todo.style.display="none";
+                }
+                break;
+        }
+    });
+}
+
+todoList.addEventListener("click", deleteTodo);
 todoButton.addEventListener('click',addTodo);
+filterOption.addEventListener('click',filterTodo);
 
