fbe8785813c57b7c208798d56159239711187185
gbenedetti22 <gabriele.benedetti22@gmail.com>
2020-09-22 22:45:10 +0200

    Progetto finito e funzionante
    - Aggiunta funzionalità di salvataggio in locale

--- app.js
+++ app.js
@@ -3,21 +3,18 @@ const todoButton=document.querySelector('.todo-button');
 const todoList=document.querySelector('.todo-list');
 const filterOption=document.querySelector('.filter-todo');
 
-function addTodo(event) {
-    event.preventDefault();
-
-    if(todoInput.value === ""){
-        alert("Hey devi inserire qualcosa da fare!");
-        return;
-    }
+function createTodo(saveLocal, todo){
     const todoDiv=document.createElement('div');
     todoDiv.classList.add("todo");
 
     const newTodo = document.createElement('li');
-    newTodo.innerText=todoInput.value;
+    newTodo.innerText=capitalize(todo);
     newTodo.classList.add("todo-item");
     todoDiv.appendChild(newTodo);
 
+    if(saveLocal) {
+        saveLocalTodo(todoInput.value);
+    }
     //Aggiungere un Button per il completamento
     const completedButton = document.createElement("button");
     completedButton.innerHTML= "<i class='fas fa-check'></i>"
@@ -33,6 +30,17 @@ function addTodo(event) {
     todoList.appendChild(todoDiv);
     todoInput.value ="";
 }
+
+function addTodo(event) {
+    event.preventDefault();
+
+    if(todoInput.value === ""){
+        alert("Hey devi inserire qualcosa da fare!");
+        return;
+    }
+    createTodo(true, todoInput.value);
+    console.log("ciao");
+}
 function deleteTodo(event) {
     const item=event.target;
     if(item.classList[0]==="trash-btn"){
@@ -40,13 +48,13 @@ function deleteTodo(event) {
         item.parentElement.addEventListener("transitionend",function(){
             item.parentElement.remove();
         });
+        deleteTodoFromStorage(item.parentElement);
     }
 
     if(item.classList[0]=== "complete-btn"){
         item.parentElement.classList.toggle("completed");
     }
 }
-
 function filterTodo(e){
     const todos=todoList.childNodes;
     todos.forEach(function(todo){
@@ -72,7 +80,44 @@ function filterTodo(e){
     });
 }
 
+document.addEventListener('DOMContentLoaded', getTodos);
 todoList.addEventListener("click", deleteTodo);
 todoButton.addEventListener('click',addTodo);
 filterOption.addEventListener('click',filterTodo);
 
+function saveLocalTodo(todo) {
+    let todos;
+    if (localStorage.getItem('todos')=== null){
+        todos=[];
+    }else{
+        todos = JSON.parse(localStorage.getItem('todos'));
+    }
+    todos.push(todo);
+    localStorage.setItem('todos', JSON.stringify(todos));
+}
+function deleteTodoFromStorage(todo){
+    let todos;
+    if (localStorage.getItem('todos')=== null){
+        todos=[];
+    }else{
+        todos = JSON.parse(localStorage.getItem('todos'));
+    }
+    const todoText = todo.children[0].innerText;
+    todos.splice(todos.indexOf(todoText),1);
+    localStorage.setItem('todos',JSON.stringify(todos));
+}
+function getTodos(){
+    let todos;
+    if (localStorage.getItem('todos')=== null){
+        todos=[];
+    }else{
+        todos = JSON.parse(localStorage.getItem('todos'));
+    }
+    todos.forEach(function(todo){
+        createTodo(false,todo);
+    });
+}
+const capitalize = (s) => {
+    if (typeof s !== 'string') return ''
+    return s.charAt(0).toUpperCase() + s.slice(1)
+}
